11ff1bca43faf5f733c9c55c51929658134f5217
Pavel Shnayder <pavel_shnayder@3divi.com>
2013-11-22 09:22:18 +0600

    flash

--- HeartRateMonitor/src/com/shnayder/heartratemonitor/CameraPreview.java
+++ HeartRateMonitor/src/com/shnayder/heartratemonitor/CameraPreview.java
@@ -1,11 +1,14 @@
 package com.shnayder.heartratemonitor;
 
 import java.io.IOException;
+import java.util.List;
 
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -21,10 +24,24 @@ public class CameraPreview extends SurfaceView implements
 		// TODO Auto-generated constructor stub
 
 		_camera = camera;
+		Parameters camPam = _camera.getParameters();
+//		camPam.setFlashMode(Camera.Parameters.FLASH_MODE_ON);
+//		_camera.setParameters(camPam);
+		
+		List<String> flashModes = camPam.getSupportedFlashModes();
+		for (String string : flashModes) {
+			Log.i(HeartRateMonitor.HRM_TAG, string);
+		}
 
 		_holder = getHolder();
 		_holder.addCallback(this);
 		_holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+	
+	private void setFlashOn(){
+		Parameters camPam = _camera.getParameters();
+		camPam.setFlashMode(Camera.Parameters.FLASH_MODE_ON);
+		_camera.setParameters(camPam);
 	}
 
 	@Override
@@ -43,7 +60,9 @@ public class CameraPreview extends SurfaceView implements
 
 		try {
 			_camera.setPreviewDisplay(_holder);
+			setFlashOn();
 			_camera.startPreview();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -55,7 +74,9 @@ public class CameraPreview extends SurfaceView implements
 
 		try {
 			_camera.setPreviewDisplay(_holder);
+			setFlashOn();
 			_camera.startPreview();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- HeartRateMonitor/src/com/shnayder/heartratemonitor/HeartRateMonitor.java
+++ HeartRateMonitor/src/com/shnayder/heartratemonitor/HeartRateMonitor.java
@@ -7,48 +7,60 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.widget.FrameLayout;
 
-public class HeartRateMonitor extends Activity
-{
-	public String HRM_TAG = "HeartRateMonitor";
-	
+public class HeartRateMonitor extends Activity {
+	public static String HRM_TAG = "HeartRateMonitor";
+
 	private boolean hasCamera = false;
 	private Camera _camera = null;
 	private CameraPreview _cameraPreview = null;
-	
-	private boolean checkCameraHardware(Context context){
-		if(!context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+
+	private boolean checkCameraHardware(Context context) {
+		if (!context.getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA)) {
 			return false;
 		}
 		return true;
 	}
 
-	public static Camera getCamera(){
+	public static Camera getCamera() {
 		Camera c = null;
 		try {
 			c = Camera.open();
-			
+
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 		return c;
 	}
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        hasCamera = checkCameraHardware(getBaseContext());
-        
-        _camera = getCamera();
-        
-        if(_camera == null)
-        	return;
-        
-        _cameraPreview = new CameraPreview(this, _camera);
-        FrameLayout framePreview = (FrameLayout)findViewById(R.id.cameraPreview);
-        framePreview.addView(_cameraPreview);
-    }
+
+	private void releaseCamera() {
+		if (_camera == null)
+			return;
+		_camera.release();
+		_camera = null;
+	}
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		hasCamera = checkCameraHardware(getBaseContext());
+
+		_camera = getCamera();
+
+		if (_camera == null)
+			return;
+
+		_cameraPreview = new CameraPreview(this, _camera);
+		FrameLayout framePreview = (FrameLayout) findViewById(R.id.cameraPreview);
+		framePreview.addView(_cameraPreview);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		releaseCamera();
+	}
 }
