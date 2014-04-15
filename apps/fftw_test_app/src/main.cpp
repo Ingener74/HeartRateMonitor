77a0621c57f8c471dea67e548d99bf5319ca8357
Ingener74 <shnaiderpasha@gmail.com>
2014-04-15 22:35:11 +0600

    understanding fft output interpretation

--- apps/fftw_test_app/src/main.cpp
+++ apps/fftw_test_app/src/main.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <limits>
 #include <stdexcept>
@@ -57,30 +58,44 @@ int main(int argc, char **argv) {
     try {
 
         using namespace boost;
-
-//        const int N = 256;
-        const int N = 2048;
-//        const int N = 2048*2;
-
-        std::vector<double> in(N), out(N);
-
-        double f1 = -M_PI, f2 = M_PI, fs = (f2 - f1) / N;
+        using namespace std;
 
         /*
-         * http://stackoverflow.com/questions/4364823/how-to-get-frequency-from-fft-result
+         * Nyquist frequency
+         * A frequency equal to half the current sample rate, which determines
+         * the highest reproducible audio frequency for that rate.
          *
+         * For example, audio CDs use a sample rate of 44,100 Hz because the
+         * resulting Nyquist frequency is 22,050 Hz—just above the limit of
+         * human hearing, 20,000 Hz. For the best audio quality, record and
+         * edit at higher sample rates and then convert down if needed.
          */
-        double sampleFreq = 1 / fs;
-        std::cout << "sample freq = " << sampleFreq << std::endl;
-        std::cout << "fft prec = " << sampleFreq / N << std::endl;
+
+        const int N = 128;
+
+        const double sample_rate = 44100 /* sample per second */;
+
+        const double frequency = sample_rate / 2 /* Hz */;
+
+        const double freq_prec = frequency / N; /* Hz in one dft output bin */
+
+        const double sample_len = N / sample_rate;
+
+        cout.precision(5);
+        cout << "sample rate = " << setw(20) << sample_rate << " sample per second" << endl;
+        cout << "frequency   = " << setw(20) << frequency << " Hz" << endl;
+        cout << "freq prec   = " << setw(20) << freq_prec << " Hz in one fft output bin" << endl;
+        cout << "sample len  = " << setw(20) << sample_len << " second" << endl;
+
+        vector<double> in(N), out(N);
+        double f1 = -M_PI, f2 = M_PI, fs = (f2 - f1) / N;
 
         for(int i = 0; i < N; ++i){
-//            in[i] = sin(f1 + fs*i*16) + 1.7*cos(f1 + fs*i*32);
-            in[i] = sin(f1 + fs*i*70);
+            in[i] = sin(f1 + fs*i*2);
         }
 
-        shared_ptr<fftw_complex[]> fftin(new fftw_complex[N]);
-        shared_ptr<fftw_complex[]> fftout(new fftw_complex[N]);
+        boost::shared_ptr<fftw_complex[]> fftin(new fftw_complex[N]);
+        boost::shared_ptr<fftw_complex[]> fftout(new fftw_complex[N]);
 
         for(int i = 0; i < N; ++i){
             fftin.get()[i][0] = in[i];
@@ -101,20 +116,23 @@ int main(int argc, char **argv) {
         cv::Mat input(H, W, CV_8UC3, cv::Scalar(0, 0, 0));
         draw_vector(in, input, cv::Scalar(255, 0, 0));
 
+        double ds = 0.0;
         for(int i = 0; i < N; ++i){
             out[i] = sqrt(
                     fftout.get()[i][0]*fftout.get()[i][0] +
                     fftout.get()[i][1]*fftout.get()[i][1]);
-//            out[i] = fftout.get()[i][0];
+
+            cout << setw(4) << i << ") " << setw(10) << ds << " - " << setw(10) << (ds + freq_prec) << " -> " << setw(15) << out[i] << endl;
+            ds += freq_prec;
         }
 
         cv::Mat freq(H, W, CV_8UC3, cv::Scalar(0, 0, 0));
         draw_vector(out, freq, cv::Scalar(0, 255, 0));
 
-//        cv::imshow("signal", input);
-//        cv::imshow("freq",   freq);
-//
-//        cv::waitKey(-1);
+        cv::imshow("signal", input);
+        cv::imshow("freq",   freq);
+
+        cv::waitKey(-1);
 
     } catch (const std::exception& e) {
         std::cerr << "exception: " << e.what() << std::endl;
